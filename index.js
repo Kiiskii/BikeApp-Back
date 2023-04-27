ff9df6f26f0c08a4dfd4596856af9f12209e80a2
Anton <antonoliverkiiski@gmail.com>
2023-04-27 20:26:20 +0300

    Swap database from mongo to postgresql

--- index.js
+++ index.js
@@ -1,60 +1,36 @@
 const express = require("express");
-const app = express();
-
-require("dotenv").config();
-
-app.get("/", (req, res) => {
-  res.send("Hello World! I am fat.");
-});
-
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.mongoURL;
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
-const journeysSchema = new mongoose.Schema({
-  Departure: String,
-  Return: String,
-  DepartureStationId: Number,
-  DepartureStationName: String,
-  ReturnStationId: Number,
-  ReturnStationName: String,
-  CoveredDistance: Number,
-  Duration: Number,
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "bikeapp",
+  password: "postgres",
+  port: 5432,
 });
 
-const Journey = mongoose.model("Journeys", journeysSchema);
-
-// journeysSchema.set("toJSON", {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString();
-//     delete returnedObject._id;
-//     delete returnedObject.__v;
-//   },
-// });
-
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extneded: true,
   })
 );
-
-app.get("/journeys", async (req, res) => {
-  const journeys = await Journey.find({}).limit(5);
-  return res.status(200).json(journeys);
+app.get("/", (request, response) => {
+  // pool.query("SELECT * FROM journeys WHERE id = 6", (error, result) => {
+  pool.query(
+    "SELECT * FROM journeys FETCH FIRST 100 ROWS ONLY",
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(result.rows);
+    }
+  );
 });
 
-app.listen(3000, function () {
-  console.log("App is running on Port 3000");
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
 });

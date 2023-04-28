46fd2d358f0a5c1a1c7e4c546814914cb9038f03
Anton <112481379+Kiiskii@users.noreply.github.com>
2023-04-28 16:42:16 +0300

    First working version. Develop (#1)
    * Setup database with mongoDB (possible switch to postgreSQL)
    
    * Swap database from mongo to postgresql
    
    * Move backend and database to docker container
    
    * Install and implement cors

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+app.use(cors());
+
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "bikeapp",
+  password: "postgres",
+  port: 5432,
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extneded: true,
+  })
+);
+app.get("/api/journeys", (request, response) => {
+  // pool.query("SELECT * FROM journeys WHERE id = 6", (error, result) => {
+  pool.query(
+    "SELECT * FROM journeys FETCH FIRST 100 ROWS ONLY",
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(result.rows);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});

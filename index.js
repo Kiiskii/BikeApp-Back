a227c1a5a20deace54a9c3c761c5e2ec880d502b
Anton <112481379+Kiiskii@users.noreply.github.com>
2023-05-20 16:38:14 +0300

    Develop (#6)
    * Setup database with mongoDB (possible switch to postgreSQL)
    
    * Swap database from mongo to postgresql
    
    * Move backend and database to docker container
    
    * Install and implement cors
    
    * Get data from stations table from the database
    
    * Pagination (#2)
    
    * Restrict amount of data being pushed to frontend to enable pagination.
    
    * Add funcionality to enable pagination for stations
    
    * Search (#3)
    
    * Add basic funcionality to search through the table using frontend
    
    * Add funcionality to enable search result updating while typing in the frontend
    
    * Get station details, departure count and return count. Send them to frontend. (#4)
    
    * Change serches SQL query to not care about capitalization (#5)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const res = require("express/lib/response");
 
 app.use(cors());
 
@@ -21,10 +22,15 @@ app.use(
     extneded: true,
   })
 );
+
+const PAGINATION_PAGELIMIT = 10;
+
 app.get("/api/journeys", (request, response) => {
-  // pool.query("SELECT * FROM journeys WHERE id = 6", (error, result) => {
+  const page = parseInt(request.query.page) || 1;
+  const offset = (page - 1) * PAGINATION_PAGELIMIT;
   pool.query(
-    "SELECT * FROM journeys FETCH FIRST 100 ROWS ONLY",
+    "SELECT * FROM journeys ORDER BY id ASC OFFSET $1 LIMIT $2",
+    [offset, PAGINATION_PAGELIMIT],
     (error, result) => {
       if (error) {
         throw error;
@@ -33,6 +39,120 @@ app.get("/api/journeys", (request, response) => {
     }
   );
 });
+
+app.get("/api/journeycount", (request, response) => {
+  pool.query(
+    "SELECT count(*) AS exact_count FROM journeys",
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      let pageCount = Math.trunc(
+        result.rows[0].exact_count / PAGINATION_PAGELIMIT
+      );
+      response.status(200).json(pageCount);
+    }
+  );
+});
+
+app.get("/api/stations", (request, response) => {
+  const page = parseInt(request.query.page) || 1;
+  const offset = (page - 1) * PAGINATION_PAGELIMIT;
+  pool.query(
+    "SELECT * FROM stations ORDER BY fid ASC OFFSET $1 LIMIT $2",
+    [offset, PAGINATION_PAGELIMIT],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(result.rows);
+    }
+  );
+});
+
+app.get("/api/stationcount", (request, response) => {
+  pool.query(
+    "SELECT count(*) AS exact_count FROM stations",
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      let pageCount = Math.trunc(
+        result.rows[0].exact_count / PAGINATION_PAGELIMIT
+      );
+      response.status(200).json(pageCount);
+    }
+  );
+});
+
+app.get("/api/search/:searchquery", (request, response) => {
+  const page = parseInt(request.query.page) || 1;
+  const offset = (page - 1) * PAGINATION_PAGELIMIT;
+  const search = request.params.searchquery.toLowerCase();
+  pool.query(
+    "SELECT * FROM stations WHERE (LOWER(nimi) LIKE $1 OR LOWER(namn) LIKE $1 OR LOWER(name) LIKE $1) ORDER BY id ASC OFFSET $2 LIMIT $3",
+    [`%${search}%`, offset, PAGINATION_PAGELIMIT],
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(result.rows);
+    }
+  );
+});
+
+app.get("/api/details/:detailsid", async (request, response) => {
+  const id = request.params.detailsid;
+  const stationsResult = await pool.query(
+    "SELECT * FROM stations WHERE fid = $1",
+    [id]
+  );
+  const stationname = stationsResult.rows[0].nimi;
+
+  const departureCountResult = await pool.query(
+    "SELECT count(*) FROM journeys WHERE departure_station_name = $1",
+    [stationname]
+  );
+  const returnCountResult = await pool.query(
+    "SELECT count(*) FROM journeys WHERE return_station_name = $1",
+    [stationname]
+  );
+
+  const stations = stationsResult.rows;
+  const departurecount = departureCountResult.rows[0].count;
+  const returncount = returnCountResult.rows[0].count;
+
+  console.log(stations);
+  console.log(departurecount);
+  console.log(returncount);
+
+  response.status(200).json({ stations, departurecount, returncount });
+});
+
+// app.get("/api/details/:detailsid", (request, response) => {
+//   const id = request.params.detailsid;
+//   console.log(id);
+//   pool.query("SELECT * FROM stations WHERE fid = $1", [id], (error, result) => {
+//     if (error) {
+//       throw error;
+//     }
+//     let stationName = result.rows[0].nimi;
+//     console.log(stationName);
+//     response.status(200).json(result.rows);
+//   });
+//   pool.query(
+//     "SELECT count(*) FROM journeys WHERE departure_station_name = $1",
+//     [stationName],
+//     (error, result) => {
+//       if (error) {
+//         throw error;
+//       }
+//       const count = result.rows;
+//       console.log(count);
+//       response.status(200).json(count);
+//     }
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);

acfe00cb02d0b70ecf91d9df78e920e3398309bb
Anton <antonoliverkiiski@gmail.com>
2023-05-21 21:32:40 +0300

    Fix code that went missing in merge

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./index.js");
-const port = 3000;
+const port = process.env.PORT ?? 3000;
+const host = !!process.env.PORT ? "0.0.0.0" : "app";
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`App running on port ${port}.`);
 });
